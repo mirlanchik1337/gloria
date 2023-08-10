4bdf993d80c0a381aadd60daf24a71c0239b9ba7
Mirlan <sadambekovmirlan9@gmail.com>
2023-08-10 23:40:12 +0600

    add banners

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.cart.models import CartItem, FavoriteProduct
+from apps.cart.models import CartItem, FavoriteProduct, Banners
 
 admin.site.register(CartItem)
 admin.site.register(FavoriteProduct)
+admin.site.register(Banners)
