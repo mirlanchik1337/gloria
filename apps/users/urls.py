4774fb43886e14c4ce2336c09e8b9b96c4da6c13
Mirlan <sadambekovmirlan9@gmail.com>
2023-08-11 22:41:18 +0600

    token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from . import views
 
@@ -58,9 +59,9 @@ urlpatterns = [
     # path("profile/", views.ProfileAPIView.as_view(), name="profile-list"),
     # path("profile/<str:id>/", views.ProfileDetailAPIView.as_view(), name="profile-update,delete"),
     #
-    # path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    # path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     *router.urls,
     # path("register/", views.UserRegistrationView.as_view(), name="user-registration"),
     # path("login/", views.UserLoginUserAPIView.as_view(), name="user-authorization"),
