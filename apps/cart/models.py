c5db8ed9b75a641e3f36d56845504902998b517f
SoupAs <sadambekovmirlan999@gmail.com>
2023-08-24 19:55:01 +0600

    push

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -6,7 +6,7 @@ from apps.product.models import Product, Category
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
